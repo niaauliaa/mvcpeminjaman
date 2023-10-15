8effa3d3c7320842cda6ba94a7233a8a70dbdbf8
niaauliaa <115599462+niaauliaa@users.noreply.github.com>
2023-10-15 21:14:07 +0700

    Add files via upload

---
+++ mvc_peminjaman/app/config/Config.php
@@ -0,0 +1,8 @@
+<?php
+// konstanta = nilai nya tidak bisa berubah
+define('BASE_URL' , 'http://localhost/mvc_peminjaman/public') ;
+
+define('DB_HOST' , 'localhost') ;
+define('DB_USER' , 'root') ;
+define('DB_PASS' , '') ;
+define('DB_NAME' , 'db_mvc_1') ;
---
+++ mvc_peminjaman/app/init.php
@@ -0,0 +1,10 @@
+<?php
+// memanggil suatu file tersebut, lalu dalam file tersebut memanggil semua file2 lain yang akan digunakan
+
+require_once 'core/App.php' ;
+require_once 'core/Controller.php' ;
+require_once 'config/Config.php' ;
+require_once 'core/Database.php' ;
+
+
+$app = new APP() ;
